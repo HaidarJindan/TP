dcce90b72a60f85a5bfd420096294d35865f62ef
Haidar341 <91077398+Haidar341@users.noreply.github.com>
2021-10-02 20:54:31 +0700

    TP.java

---
+++ TP.java
@@ -0,0 +1,11 @@
+public class TP{
+  public static void main(String[] args) {
+	        //Jumlah Tabungan Dira Selama x hari 
+    int tabungan = 100000;
+	System.out.println("tabungan = 100000");
+    int hari = 30;
+	System.out.println("hari = 30");
+    System.out.println(tabungan * hari);
+    
+     }
+}
